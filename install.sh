ddc0108208dbd015c07b8ca70f7911ed299d8681
myhailo.rudenko <myhailo.rudenko@gmail.com>
2023-02-15 16:26:41 +0200

    Update install script, set new version

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-echo 'Copy pyZender script'
+echo 'Upgrade pyzender pip module'
+pip install ../pyzender
+
+echo 'Copy pyzender script to /opt/pyzender/agent.py'
 mkdir /opt/pyzender 2> /dev/null
 cp agent.py /opt/pyzender/agent.py
 chmod 644 /opt/pyzender/agent.py
 
-echo 'Create pyZender service'
-sudo bash -c 'cat << 'EOF' >/lib/systemd/system/pyzender.service
+echo 'Copy pyzender service to /lib/systemd/system/pyzender.service'
+bash -c 'cat << 'EOF' >/tmp/__pyzender.service
 [Unit]
 Description=pyZender service
 After=multi-user.target
@@ -19,11 +22,13 @@ User=${USER}
 [Install]
 WantedBy=multi-user.target
 EOF'
+
+sudo mv /tmp/__pyzender.service /lib/systemd/system/pyzender.service
 sudo chmod 644 /lib/systemd/system/pyzender.service
 
 echo 'Reload systemd'
 sudo systemctl daemon-reload
 sudo systemctl enable pyzender.service
 
-echo 'Starting PyZender service'
-sudo systemctl start pyzender.service
+echo 'Restart pyzender service'
+sudo systemctl restart pyzender.service
