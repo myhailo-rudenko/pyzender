62c9fafb20025dfed5e5263eb008acc0e8888b71
myhailo.rudenko <myhailo.rudenko@gmail.com>
2023-02-27 12:20:55 +0200

    Fix upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -5,5 +5,7 @@ rm /opt/pyzender/pyzender-latest -rf 2> /dev/null
 echo "Download the latest version of pyzender"
 wget https://github.com/myhailo-rudenko/pyzender/archive/refs/tags/latest.tar.gz -P /opt/pyzender/
 
+tar xvf /opt/pyzender/latest.tar.gz
+
 cd /opt/pyzender/pyzender-latest
 . install.sh
